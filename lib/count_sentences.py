21fa8c8319f544bbcfdabda491f8014dd2661230
Emmanuel Leakono <leakonoemmanuel3@gmail.com>
2024-05-23 00:31:41 +0300

    counting sequence

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,32 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self._value = None  # Initial internal value
+        self.value = value  # Use the property setter
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        if isinstance(value, str):
+            self._value = value
+        else:
+            print("The value must be a string.")
+
+    def is_sentence(self):
+        return self.value.endswith('.')
+
+    def is_question(self):
+        return self.value.endswith('?')
+
+    def is_exclamation(self):
+        return self.value.endswith('!')
+
+    def count_sentences(self):
+        sentences = [s for s in self.value.split() if s.endswith(('.', '?', '!'))]
+        return len(sentences)
+
+
